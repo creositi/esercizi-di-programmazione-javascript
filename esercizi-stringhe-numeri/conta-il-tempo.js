1a4b498dc3c40481bb9d9735ee9494990c7a24c4
creositi <info@creositi.com>
2017-05-29 14:46:44 +0200

    soluzione

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -6,3 +6,15 @@
     Output: 3 ore, 29 minuti e 20 secondi.
   http://www.imparareaprogrammare.it
 */
+
+var seconds = 12560;
+var hours = seconds / 3600; // ore
+var hours2 = Math.floor(hours); // ore arrotondate
+var secondsinhours = hours2 * 3600; // quanti secondi delle ore arrotondate
+var rest = seconds - secondsinhours; // quanti secondi rimangono
+var minuts = rest / 60; // minuti
+var minuts2 = Math.floor(minuts); // minuti arrotondati
+var secondsinminuts = minuts2 * 60; // quanti secondi dei minuti arrotondati
+var seconds2 = seconds - secondsinhours - secondsinminuts; // secondi
+
+console.log(hours2, 'ore,', minuts2, 'minuti,', seconds2, 'secondi');

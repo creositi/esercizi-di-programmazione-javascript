787cc8402ab3c97362d3d514ff6b02a24533cae9
creositi <info@creositi.com>
2017-06-03 12:45:29 +0200

    solution

--- esercizi-condizioni/pari-o-dispari.js
+++ esercizi-condizioni/pari-o-dispari.js
@@ -3,3 +3,12 @@
   Scrivi un programma che prenda in input un numero intero e restituisca 0 se è pari o 1 se è dispari.
   http://www.imparareaprogrammare.it
 */
+
+var number = 10;
+
+if (number % 2 == 0)
+    console.log('0');
+else if (number % 2 == 1)
+    console.log('1');
+else
+    console.log('non è un numero');

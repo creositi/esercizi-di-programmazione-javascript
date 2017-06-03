650730c986798643b40c5b26bb7d0e1e27fd2a16
creositi <info@creositi.com>
2017-06-03 12:36:14 +0200

    solution

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -13,3 +13,18 @@
   Realizzare una versione con il costrutto Switch-Case.
   http://www.imparareaprogrammare.it
 */
+
+var voto = 1;
+
+if (voto <= 18)
+    console.log('insufficiente');
+else if (voto <= 21)
+    console.log('sufficiente');
+else if (voto <= 24)
+    console.log('buono');
+else if (voto <= 27)
+    console.log('distinto');
+else if (voto <= 29)
+    console.log('ottimo');
+else if (voto >= 30)
+    console.log('eccellente');

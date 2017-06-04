0cae5791850914696d17d3098059a4f299bebf19
creositi <info@creositi.com>
2017-06-04 20:57:07 +0200

    solution

--- esercizi-condizioni/menu-dei-dolci.js
+++ esercizi-condizioni/menu-dei-dolci.js
@@ -10,3 +10,21 @@
   Se la scelta non è tra quelle elencate verrà stampata la scritta 'Dolce non disponibile' altrimenti verrà visualizzata la scelta effettuata 'Hai scelto il dolce X'.
   http://www.imparareaprogrammare.it
 */
+var menu = 5;
+
+switch (menu) {
+    case 1:
+        console.log('Hai scelto il Tiramisu');
+        break;
+    case 2:
+        console.log('Hai scelto la torta della nonna');
+        break;
+    case 3:
+        console.log('Hai scelto il Cheesecake alla nutella');
+        break;
+    case 4:
+        console.log('Hai scelto la Macedonia');
+        break;
+    default:
+        console.log('Dolce non disponibile');
+}

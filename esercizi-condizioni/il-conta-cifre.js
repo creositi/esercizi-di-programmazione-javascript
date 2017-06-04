f95ad9517601fdeb1982bcc3c13dd5c962a35089
creositi <info@creositi.com>
2017-06-04 21:07:20 +0200

    solution

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -3,3 +3,9 @@
   Scrivi un programma che calcoli quante cifre sono contenute in un numero inserito in input (massimo 9999).
   http://www.imparareaprogrammare.it
 */
+
+var number = '12';
+var number2 = number.length;
+
+if (number2 <= 9999)
+    document.write(number2);

00b624c450571ecfd2c33a950150d86d83455306
creositi <info@creositi.com>
2017-06-05 11:31:58 +0200

    Update la-calcolatrice.js

--- esercizi-condizioni/la-calcolatrice.js
+++ esercizi-condizioni/la-calcolatrice.js
@@ -14,3 +14,31 @@
   Utilizza il costrutto Switch-Case mediante acquisizione di caratteri.
   http://www.imparareaprogrammare.it
 */
+
+var operation = 'media';
+
+switch (operation) {
+    case 'somma':
+        console.log(first + second);
+        break;
+    case 'sottrazione':
+        console.log(first - second);
+        break;
+    case 'moltiplicazione':
+        console.log(first * second);
+        break;
+    case 'divisione':
+        console.log(first / second);
+        break;
+    case 'modulo':
+        console.log(first % second);
+        break;
+    case 'potenza':
+        console.log(Math.pow(first, second));
+        break;
+    case 'media':
+        console.log((first + second)/2);
+        break;
+    default:
+        console.log('nessuna opearazione');
+}

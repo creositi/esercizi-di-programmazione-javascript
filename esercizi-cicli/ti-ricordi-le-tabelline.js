863ce2dee335f9d41988b490b277097cea0791cb
creositi <info@creositi.com>
2017-06-06 23:10:42 +0200

    solution

--- esercizi-cicli/ti-ricordi-le-tabelline.js
+++ esercizi-cicli/ti-ricordi-le-tabelline.js
@@ -3,3 +3,9 @@
   Scrivi un programma che stampi che prenda in input un numero e stampi la tabellina corrispondente.
   http://www.imparareaprogrammare.it
 */
+var number = 3;
+var limit = 11;
+
+for (i = 0; i < limit; i++) {
+    console.log(number * i);
+}

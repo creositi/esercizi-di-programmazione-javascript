625b3b81db9bf55a0b3a0ca4f636c47a3559ffd4
creositi <info@creositi.com>
2017-06-06 23:52:10 +0200

    solution

--- esercizi-cicli/rincorri-la-decina.js
+++ esercizi-cicli/rincorri-la-decina.js
@@ -4,3 +4,14 @@
   Hint: per andare a capo stampa '\n'.
   http://www.imparareaprogrammare.it
 */
+
+var limit = 100;
+var number = '';
+
+for (i = 0; i <= limit; i++) {
+    number = number + i + ' ';
+    if (i % 10 == 0)
+        number = number + '\n';
+}
+
+console.log(number);

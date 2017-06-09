f99ec162d0b4a38b6640db255b505e7e24e92a2b
creositi <info@creositi.com>
2017-06-09 10:28:20 +0200

    solution

--- esercizi-cicli/il-calendario-mediocre.js
+++ esercizi-cicli/il-calendario-mediocre.js
@@ -8,3 +8,43 @@
   Piuttosto che passarein input i giorni del mese passa direttamente il mese e calcola tu i giorni corrispondenti.
   http://www.imparareaprogrammare.it
 */
+var days = 30;
+var day = '';
+var sett = 3;
+
+for (var i = 1; i <= days; i++) {
+    if (sett === 7) 
+        sett = 0;
+
+    switch (sett) {
+        case 0:
+            day = 'lunedi';
+            break;
+        case 1:
+            day = 'martedi';
+            break;
+        case 2:
+            day = 'mercoledi';
+            break;
+        case 3:
+            day = 'giovedi';
+            break;
+        case 4:
+            day = 'venerdi';
+            break;
+        case 5:
+            day = 'sabato';
+            break;
+        case 6:
+            day = 'domenica';
+            break;
+    }
+    if (sett === 6)
+        document.write(i + ' ' + day + '<br />');
+    else if (i === days)
+        document.write(i + ' ' + day + '<br />');
+    else
+        document.write(i + ' ' + day + ', ');
+
+    sett = sett + 1;
+}

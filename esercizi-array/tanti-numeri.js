d13ae1472da3c530572c67688cf0b8fb97847fec
creositi <info@creositi.com>
2017-06-09 16:07:34 +0200

    solution

--- esercizi-array/tanti-numeri.js
+++ esercizi-array/tanti-numeri.js
@@ -4,3 +4,22 @@
   Stampa anche quanti sono i valori monori della media e quanti quelli maggiori.
   http://www.imparareaprogrammare.it
 */
+
+
+var numbers = [1, 2, 3, 4, 5];
+var tot = 0;
+
+
+for (i = 1; i <= numbers.length; i++) {
+  tot = tot + i;
+  var tot2 = tot / numbers.length;
+}
+
+var filtered = numbers.filter(function (item) {
+    return item < tot2;
+}
+);
+
+var filtered2 = numbers.length - filtered.length;
+
+console.log(`La media dei numeri è ${tot2}. I valori minori della media sono ${filtered.length}. I valori maggiori della media sono ${filtered2}`)

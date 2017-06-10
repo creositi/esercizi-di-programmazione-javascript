e9f65b0cd1711d14973981924380074f7dbe95ca
creositi <info@creositi.com>
2017-06-10 16:01:10 +0200

    solution

--- esercizi-array/azzecca-e-azzera.js
+++ esercizi-array/azzecca-e-azzera.js
@@ -6,3 +6,20 @@
   Hint: Per generare un numero casuale utlizza la funzione javascript random, che restituisce un intervallo compreso tra 0 e 1 che necessita di essere convertito per il tuo intervallo.
   http://www.imparareaprogrammare.it
 */
+
+var arr = [];
+var arr3 = [];
+var number = [2,3,1];
+
+
+for(i = 0; i < 100; i++) {
+    arr[i] = Math.floor( Math.random() * 50 );
+    var arr2 = arr[i];
+    arr3 = arr2;
+      if (arr2 % number[0] === 0 || arr2 % number[1] === 0 || arr2 % number[2] === 0)
+      arr[i] = 0;
+
+console.log(arr3);
+}
+
+console.log(arr);

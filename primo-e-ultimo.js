08f9d0823a5c507f36885a8f388008ba1b7edcd8
creositi <info@creositi.com>
2017-06-15 20:15:48 +0200

    solution

---
+++ primo-e-ultimo.js
@@ -0,0 +1,14 @@
+var n1 = 115;
+var n2 = 2143;
+var n3 = 10031;
+
+
+function maxnum() {
+  return Math.max(n1,n2,n3);
+}
+    console.log(maxnum());
+
+function minnum() {
+  return Math.min(n1,n2,n3);
+}
+    console.log(minnum());
